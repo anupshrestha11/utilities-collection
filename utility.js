1a42cce067799893513a7f4180bc90e3f59fafef
Anup Shrestha <anupshresth9@gmail.com>
2022-04-02 23:34:22 +0545

    ADDED: debouce and throttle utility functions

---
+++ utility.js
@@ -0,0 +1,66 @@
+
+// Debounce - best for input
+/**
+ *
+ * @param {function} callback
+ * @param {integer-> milliseconds} delay
+ * @returns callback function
+ */
+function debounce(callback, delay = 1000) {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+const updateDebounce = debounce(() => {
+  // implement the desired code
+  console.log("update debouce");
+});
+
+
+
+
+
+// throttle - best for continuous updates with delay
+/**
+ *
+ * @param {fucntion} callback
+ * @param {integer -> milliseconds} delay
+ * @returns
+ */
+function throttle(callback, delay = 1000) {
+  let shouldWait = false;
+  let waitingArgs;
+
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      callback(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    callback(...args);
+    shouldWait = true;
+
+    setTimeout(timeoutFunc, delay);
+  };
+}
+
+const updateThrottle = throttle(() => {
+  // implement the desired code
+  console.log("update throtte");
+});
